f051d195153ed43f0aa0da6dcafc132008bfabc7
sgostanyan <sgostanyan@gmail.com>
2020-11-18 00:15:27 +0100

    list page module

--- web/modules/custom/list_page/src/Controller/ArticleListPageController.php
+++ web/modules/custom/list_page/src/Controller/ArticleListPageController.php
@@ -11,12 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class ArticleListPageController extends ListPageController {
 
+  // Config entity.
   const CONFIG_ENTITY_TYPE = 'node';
 
   const CONFIG_BUNDLE_FIELD = 'type';
 
   const CONFIG_BUNDLE_ID = 'article';
 
+  // Contents to list.
   const CONTENT_ENTITY_TYPE = 'node';
 
   const CONTENT_BUNDLE_FIELD = 'type';
@@ -40,13 +42,21 @@ class ArticleListPageController extends ListPageController {
   public function page(Request $request) {
     parent::page($request);
 
+    // Initiate list manager.
+    $lisPageManager = $this->listPageManager->create(self::CONFIG_ENTITY_TYPE, self::CONFIG_BUNDLE_FIELD, self::CONFIG_BUNDLE_ID);
+
+    // Values from config entity.
+    $configValues = $this->listPageManager->getConfigValues();
+
+    // Initiate query parameter is pager is set.
     if (self::PAGER !== NULL && $request->get('page') === NULL) {
       $request->query->set('page', 0);
     }
 
-    $lisPageManager = $this->listPageManager->create(self::CONFIG_ENTITY_TYPE, self::CONFIG_BUNDLE_FIELD, self::CONFIG_BUNDLE_ID);
+    // Start search. Use buildConditions() for adding extra conditions.
     $result = $lisPageManager->getResults(self::CONTENT_ENTITY_TYPE, $this->buildConditions(), self::PAGER);
 
+    // Render.
     $render = [];
     $render[] = [
       '#theme' => 'main_page',
@@ -61,7 +71,7 @@ class ArticleListPageController extends ListPageController {
 
   /**
    * @param array|null $conditions
-   *  Array of array [field, value, operator]
+   *  Array of array [fieldName, value, operator]
    * ex:
    * [
    *  ['field_category', ['34', '23', '22'], 'IN'],
@@ -74,6 +84,7 @@ class ArticleListPageController extends ListPageController {
    */
   public function buildConditions(array $conditions = []) {
     parent::buildConditions($conditions);
+    // Default condition.
     $builtConditions = [
       [
         self::CONFIG_BUNDLE_FIELD,
@@ -81,6 +92,7 @@ class ArticleListPageController extends ListPageController {
         'IN',
       ],
     ];
+    // Extra conditions.
     foreach ($conditions as $condition) {
       $builtConditions[] = $condition;
     }
