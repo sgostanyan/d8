9e4cbe8a054b4dbc724ed3e7b4f16b7261401aaf
sgostanyan <sgostanyan@gmail.com>
2021-01-20 22:01:02 +0100

    graphql

---
+++ web/modules/custom/d8_global/src/Controller/graphql.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Drupal\d8_global\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class TestController.
+ */
+class TestController extends ControllerBase {
+
+  /**
+   * Test1.
+   *
+   * @return array Return Hello string.
+   *   Return Hello string.
+   */
+  public function test1() {
+// curl \
+    //-X POST \
+    //-H "Content-Type: application/json" \
+    //--data '{ "query": "{ countries { code name } } " }' \
+    //https://countries.trevorblades.com/
+
+
+    // curl -X POST -H "Content-Type: application/json" --data '{ "query": "{ user(id: 1) { id name } }" }' https://graphqlzero.almansi.me/api
+
+    $query = <<<'GRAPHQL'
+{
+  user(id: 1) {
+    id
+    name
+  }
+}
+GRAPHQL;
+
+   $result =  $this->graphql_query('https://graphqlzero.almansi.me/api', $query, ['id' => 1], null);
+
+   $stop='';
+   // $this->testWS();
+  //  $this->sgEntityServices();
+
+    /**
+     * @var \Drupal\sg_entity_services\Services\SgEntityServices $sgServices
+     */
+  ///  $sgServices = \Drupal::service('sg_entity_services.service');
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Implement method: test1'),
+    ];
+  }
+
+  public function graphql_query(string $endpoint, string $query, array $variables = [], string $token = null) {
+    $headers = [
+      'Content-Type: application/json',
+    ];
+    if (NULL !== $token) {
+      $headers[] = "Authorization: bearer $token";
+    }
+
+    if (FALSE === $data = @file_get_contents($endpoint,
+        FALSE,
+        stream_context_create([
+          'http' => [
+            'method' => 'POST',
+            'header' => $headers,
+            'content' => json_encode([
+              'query' => $query,
+              //'variables' => $variables
+            ]),
+          ]
+        ]))) {
+      $error = error_get_last();
+      var_dump($error);
+      throw new \ErrorException($error['message'], $error['type']);
+    }
+
+    return json_decode($data, TRUE);
+  }
+
+
+    public function testWS() {
+    $orphee = 'https://wsorphee.gironde.fr/serviceopac.asmx';
+    $opac = 'https://wsopac.gironde.fr/service.asmx?WSDL';
+    $cle = '82ba29e9906bad99122c9b68e5552ec8';
+
+    $header = 'Host: wsorphee.gironde.fr
+  Content-Type: text/xml; charset=utf-8
+  Content-Length: length
+  SOAPAction: "http://c3rb.org/GetToken"';
+
+    $xml = '<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <GetToken xmlns="http://c3rb.org/">
+      <key>string</key>
+    </GetToken>
+  </soap:Body>
+</soap:Envelope>';
+
+    try {
+      $client = new \SoapClient($opac, [
+        'Host' => 'wsorphee.gironde.fr',
+        'Content-Type' => 'text/xml; charset=utf-8',
+        'Content-Length' => 'length',
+        'SOAPAction' => 'http://c3rb.org/GetToken',
+      ]);
+    }
+    catch (\SoapFault $e) {
+      ksm($e->getMessage());
+      return;
+    }
+
+
+    //$client = new SoapClient("http://localhost/wsOpac/ServiceOpac.asmx?WSDL");
+
+    //$client = new SoapClient($opac, ["key" => $cle]);
+    //ksm($client);
+    //$rslt = $client->__getToken(["key" => $cle]);
+    //ksm($client->__getFunctions());
+    //ksm($client->__soapCall('getToken', ["key" => $cle]));
+    //$_SESSION['orphee_id'] = $rslt->GetTokenResult;
+    //$client->__setCookie("ASP.NET_SessionId", $_SESSION['orphee_id']);
+
+  }
+
+  public function sgEntityServices() {
+    $sgEntityService = \Drupal::service('sg_entity_services.service');
+    /*$fid = $sgEntityService->getFileManager()->generateFileEntity('public://sources/', 'tiger.jpg', 'private://animals/');
+     $fileInfo = $sgEntityService->getFileManager()->getFileInfos(281);
+     $fileSize = $sgEntityService->getFileManager()->sanitizeFileSize(286567); //287 Ko
+     $image = $sgEntityService->getEntityDisplayManager()->imageStyleRender(298, 'thumbnail', ['class' => ['thumb-style']]);
+     $imageUrl = $sgEntityService->getImageManager()->getImageStyleUrl(298, 'thumbnail');
+     $imageStyles = $sgEntityService->getImageManager()->getImageStyles();*/
+
+/*     $entities = $sgEntityService->getEntityStorageManager()->getEntities('node', NULL, [4]);
+     $viewModes = $sgEntityService->getEntityDisplayManager()->getViewModes('node');
+     $renderArray = $sgEntityService->getEntityDisplayManager()->renderEntity(reset($entities['article']));
+     $markup = $sgEntityService->getEntityDisplayManager()->renderArrayToMarkup($renderArray);
+     $tag = $sgEntityService->getEntityDisplayManager()->htmlTagRender('a', 'TOTO', ['href' => 'http://top.com']);
+     $markup = $sgEntityService->getEntityDisplayManager()->renderArrayToMarkup($tag);*/
+
+     $trans = [
+       'en' => [
+         'title' => 'EN title',
+         'body' => [
+           'value' => 'English summary',
+         ],
+         'field_tags' => [
+           [
+             'target_id' => 1,
+           ],
+         ],
+       ],
+       'es' => [
+         'title' => 'ES title',
+         'body' => [
+           'value' => 'Spanish summary',
+         ],
+         'field_tags' => [
+           [
+             'target_id' => 1,
+           ],
+         ],
+       ],
+     ];
+
+     $fieldValues = [
+       'title' => 'Title',
+       'body' => [
+         'value' => 'summary text',
+       ],
+       'field_tags' => [
+         [
+           'target_id' => 1,
+         ],
+       ],
+     ];
+
+
+    $newEntity = \Drupal::service('sg_entity_services.service')
+       ->getEntityStorageManager()
+       ->createEntity('node', [
+         'type' => 'article',
+         'title' => 'TaSoeur'], $trans);
+
+    /*$transEntity = Drupal::service('sg_entity_services.service')
+      ->getEntityStorageManager()
+      ->addTranslations(reset($entities['article']), $trans);*/
+
+    //return;
+  }
+
+  public function deleteFiles() {
+    $files = \Drupal::entityTypeManager()->getStorage('file')->loadMultiple();
+    foreach ($files as $file) {
+      $file->delete();
+    }
+  }
+
+}
